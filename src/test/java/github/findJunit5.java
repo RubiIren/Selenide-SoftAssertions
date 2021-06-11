9ce9b8b4efd559bd076333df59da0f5818b4168d
RubiIren <rubinshteiniv@gmail.com>
2021-06-11 16:25:21 +0300

    Change

--- src/test/java/github/findJunit5.java
+++ src/test/java/github/findJunit5.java
@@ -5,23 +5,21 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class FindJunit5 {
+public class findJunit5 {
     @Test
-    void Junit5() {
-        Configuration.headless = false;
+    void junit5() {
         //Открыть страницу Selenide в Github
         open("https://github.com/selenide/selenide");
         //Перейдите в раздел Wiki проекта
         $(byText("Wiki")).click();
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $(byClassName("js-wiki-more-pages-link")).click();
-        $(byClassName("wiki-rightbar")).shouldHave(text("SoftAssertions"));
+        $(".js-wiki-more-pages-link").click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
        //Открыть страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(byText("SoftAssertions")).click();
-        $(byClassName("markdown-body")).shouldHave(text("Using JUnit5 extend test class"));
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
